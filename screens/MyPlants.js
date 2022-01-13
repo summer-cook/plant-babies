e2a02641d893d0e255981e3d8682f16830f059c1
summer-cook <summercook@bellsouth.net>
2022-01-13 12:25:44 -0800

    add coming soon and custom button

--- screens/MyPlants.js
+++ screens/MyPlants.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { View, TouchableOpacity, StyleSheet, } from "react-native";
-import CustomButton from "../components/Button";
+import CustomButton from "../components/CustomButton";
 
 function MyPlants({ navigation }) {
   return (
