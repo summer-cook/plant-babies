9170947548034dc529398c827d1969114cc58154
Summer Cook <summer@scientist.com>
2023-04-14 14:56:22 -0500

    add sign out

--- screens/Auth.js
+++ screens/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 import { firebase } from '../firebaseConfig'
 
@@ -6,53 +6,70 @@ export default function LoginScreen() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
+  const [isSignedIn, setIsSignedIn] = useState(false)
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setIsSignedIn(!!user)
+    })
+  }, [])
 
   const handleSignUp = () => {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
         // Get the newly created user's ID
-        const userId = userCredential.user.uid;
+        const userId = userCredential.user.uid
         // Add the user's email to the database
         firebase.database().ref(`users/${userId}`).set({
           email: email,
         }, (error) => {
           if (error) {
-            console.log('Error adding user to database:', error);
+            console.log('Error adding user to database:', error)
           } else {
-            console.log('User added to database');
+            console.log('User added to database')
           }
         })
-        console.log('User account created')
-      })
-      .catch((err) => setError(err.message))
+      console.log('User account created')
+    })
+    .catch((err) => setError(err.message))
   }
 
   const handleSignIn = () => {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(() => {
-        console.log('User signed in')
-      })
-      .catch((err) => setError(err.message))
+    firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+      console.log('User signed in')
+    })
+    .catch((err) => setError(err.message))
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut().then(() => {
+      console.log('User signed out')
+    })
+    .catch((err) => setError(err.message))
   }
 
   // TODO styling and make this 2 separate screens (or tabs? sign in and sign up should be separate)
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder='Email'
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder='Password'
-        secureTextEntry={true}
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button title='Sign up' onPress={handleSignUp} />
-      <Button title='Sign in' onPress={handleSignIn} />
+      {!isSignedIn && (
+        <>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <Button title="Sign up" onPress={handleSignUp} />
+          <Button title="Sign in" onPress={handleSignIn} />
+        </>
+      )}
+      {isSignedIn && <Button title="Sign out" onPress={handleSignOut} />}
       {error ? <Text style={styles.error}>{error}</Text> : null}
     </View>
   )
